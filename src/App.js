ebb521c5debd5152e4072c24ef81a400d79b2883
rahulsharma6611 <107174176+rahul-cubastion@users.noreply.github.com>
2022-10-17 12:48:45 +0530

    app changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Organizations from './Components/Organizations/Organizations';
-import BankDetails from "./Components/Organizations/OrganizationTable/AddNewOrganization/BankDetails";
+
 import Home from './Components/Home/Home';
 
 function App() {  
 return (
   <div className="App">
-    {/* <BrowserRouter>
+    <BrowserRouter>
         <Routes>
             <Route path = "/" element = {<Home/>}>
             <Route path="organization" element={<Organizations/>}/>
             </Route>
         </Routes>
-    </BrowserRouter> */}
+    </BrowserRouter>
 
-<BankDetails/>
+{/* <BankDetails/> */}
 
 
   </div>
