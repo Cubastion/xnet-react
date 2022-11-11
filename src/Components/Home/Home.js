a3c8822e422f0c71e4fa483755798fe2e478a773
rahulsharma6611 <107174176+rahul-cubastion@users.noreply.github.com>
2022-11-11 15:57:01 +0530

    chakra removed

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,26 +1,23 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import SideNavbar from "../Side-NavBar/SideNavbar";
-import { ChakraProvider } from "@chakra-ui/react";
 const Home = () => {
   return (
-    <ChakraProvider>
-      <div style={{ display: "flex" }}>
-        <div style={{ width: "20%" }}>
-          <SideNavbar />
-        </div>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            width: "80%",
-            marginLeft: "-10px",
-          }}
-        >
-          <Outlet />
-        </div>
+    <div style={{ display: "flex" }}>
+      <div style={{ width: "20%" }}>
+        <SideNavbar />
       </div>
-    </ChakraProvider>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          width: "80%",
+          marginLeft: "-10px",
+        }}
+      >
+        <Outlet />
+      </div>
+    </div>
   );
 };
 
